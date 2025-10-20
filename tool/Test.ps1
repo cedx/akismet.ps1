0636b34a40a7b411b8f5c8a54a346044cab8c36d
Cédric Belin <cedx@outlook.com>
2025-10-20 22:06:31 +0200

    Update the "Test" script

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -2,12 +2,5 @@
 pwsh -Command {
 	Import-Module Pester
 	Invoke-Pester test
-
-	if (Get-Module BurntToast -ListAvailable) {
-		Import-Module BurntToast
-		$message = $LASTEXITCODE -eq 0 ? "were successfully completed" : "failed with $LASTEXITCODE $($LASTEXITCODE -gt 1 ? "errors" : "error")"
-		New-BurntToastNotification -AppLogo Program.ico -Text "Akismet for PowerShell", "The tests $message." -UniqueIdentifier Belin.Akismet
-	}
-
 	exit $LASTEXITCODE
 }
