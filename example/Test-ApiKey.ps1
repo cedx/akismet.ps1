e4f4c357b09012dde5b04afb224c45a24153ea75
Cédric Belin <cedx@outlook.com>
2025-11-04 23:27:54 +0100

    Code formatting [skip ci]

--- example/Test-ApiKey.ps1
+++ example/Test-ApiKey.ps1
@@ -4,5 +4,5 @@
 #>
 Import-Module Akismet
 
-$isValid = Test-AkismetApiKey -ApiKey "123YourAPIKey" -Blog "https://www.yourblog.com"
+$isValid = Test-AkismetApiKey "123YourAPIKey" -Blog "https://www.yourblog.com"
 Write-Output ($isValid ? "The API key is valid." : "The API key is invalid.")
