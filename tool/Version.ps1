68ba487f1c7baaeac95ae12edcf0eb1bc4471ee1
Cédric Belin <cedx@outlook.com>
2025-11-13 17:23:25 +0100

    Update the "Version" script [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,4 +1,3 @@
 "Updating the version number in the sources..."
 $version = (Import-PowerShellDataFile "Akismet.psd1").ModuleVersion
 (Get-Content "src/Client.psm1") -replace 'Version = "\d+(\.\d+){2}"', "Version = ""$version""" | Out-File "src/Client.psm1"
-(Get-Content "ReadMe.md") -replace "module/v\d+(\.\d+){2}", "module/v$version" | Out-File "ReadMe.md"
