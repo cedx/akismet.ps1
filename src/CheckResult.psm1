b4706907a0ad66f4d351fc595f8704a793154abf
Cédric Belin <cedx@outlook.com>
2025-11-14 09:49:36 +0100

    Update the documentation [skip ci]

--- src/CheckResult.psm1
+++ src/CheckResult.psm1
@@ -3,7 +3,22 @@
 	Specifies the result of a comment check.
 #>
 enum CheckResult {
+
+	<#
+	.SYNOPSIS
+		The comment is a ham.
+	#>
 	Ham
+
+	<#
+	.SYNOPSIS
+		The comment is a spam.
+	#>
 	Spam
+
+	<#
+	.SYNOPSIS
+		The comment is a pervasive spam (i.e. it can be safely discarded).
+	#>
 	PervasiveSpam
 }
