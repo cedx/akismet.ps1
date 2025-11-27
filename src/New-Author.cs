e4a4e2c4241188b56726285fc8b82d7a27164504
Cédric Belin <cedx@outlook.com>
2025-11-27 22:44:01 +0100

    Port the `New-Author` cmdlet

--- src/New-Author.cs
+++ src/New-Author.cs
@@ -6,4 +6,50 @@ namespace Belin.Akismet.Cmdlets;
 [Cmdlet(VerbsCommon.New, "Author")]
 public class NewAuthor: Cmdlet {
 
+	/// <summary>
+	/// The author's mail address. If you set it to `"akismet-guaranteed-spam@example.com"`, Akismet will always return `$true`.
+	/// </summary>
+	[Parameter]
+	public string Email { get; set; } = "";
+
+	/// <summary>
+	/// The author's IP address.
+	/// </summary>
+	[Parameter(Mandatory = true)]
+	public required string IPAddress { get; set; }
+
+	/// <summary>
+	/// The author's name. If you set it to `"viagra-test-123"`, Akismet will always return `$true`.
+	/// </summary>
+	[Parameter(Position = 0)]
+	public string Name { get; set; } = "";
+
+	/// <summary>
+	/// The author's role. If you set it to `"administrator"`, Akismet will always return `$false`.
+	/// </summary>
+	[Parameter]
+	public string Role { get; set; } = "";
+
+	/// <summary>
+	/// The URL of the author's website.
+	/// </summary>
+	[Parameter]
+	public string Url { get; set; } = "";
+
+	/// <summary>
+	/// The author's user agent, that is the string identifying the Web browser used to submit comments.
+	/// </summary>
+	[Parameter]
+	public string UserAgent { get; set; } = "";
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() => WriteObject(new Author(IPAddress) {
+		Email = Email,
+		Name = Name,
+		Role = Role,
+		Url = string.IsNullOrEmpty(Url) ? null : new Uri(Url, UriKind.Absolute),
+		UserAgent = UserAgent
+	});
 }
