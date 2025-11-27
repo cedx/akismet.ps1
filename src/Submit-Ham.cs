d290db68f86efc2199518cb5d51ee0eee3c806cc
Cédric Belin <cedx@outlook.com>
2025-11-27 22:44:43 +0100

    Port the `Submit-Ham` cmdlet

--- src/Submit-Ham.cs
+++ src/Submit-Ham.cs
@@ -6,4 +6,21 @@ namespace Belin.Akismet.Cmdlets;
 [Cmdlet(VerbsLifecycle.Submit, "Ham")]
 public class SubmitHam: Cmdlet {
 
+	/// <summary>
+	/// The Akismet client used to submit the comment.
+	/// </summary>
+	[Parameter(Mandatory = true)]
+	public required Client Client { get; set; }
+
+	/// <summary>
+	/// The comment to be submitted.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+	public required Comment Comment { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() =>
+		Client.SubmitHam(Comment).GetAwaiter().GetResult();
 }
