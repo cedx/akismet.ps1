376f038ee2eb64a467fc94aa1995dcf920aff0f3
Cédric Belin <cedx@outlook.com>
2025-11-29 14:37:32 +0100

    Update the user agent string [skip ci]

--- example/Submit-Ham.ps1
+++ example/Submit-Ham.ps1
@@ -4,7 +4,7 @@
 #>
 Import-Module Akismet
 
-$author = New-AkismetAuthor -IPAddress "192.168.0.1" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0"
+$author = New-AkismetAuthor -IPAddress "192.168.0.1" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
 $comment = New-AkismetComment "I'm testing out the Service API." -Author $author
 
 $client = New-AkismetClient -ApiKey "123YourAPIKey" -Blog "https://www.yourblog.com"
