4d4a85acee9ed3aeb395b13dcec3adc48412a125
Cédric Belin <cedx@outlook.com>
2025-11-29 12:41:51 +0100

    Port the `Test-ApiKey` cmdlet [skip ci]

--- src/Test-ApiKey.cs
+++ src/Test-ApiKey.cs
@@ -7,4 +7,21 @@ namespace Belin.Akismet.Cmdlets;
 [OutputType(typeof(bool))]
 public class TestApiKey: Cmdlet {
 
+	/// <summary>
+	/// The Akismet API key.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+	public required string ApiKey { get; set; }
+
+	/// <summary>
+	/// The front page or home URL of the instance making requests.
+	/// </summary>
+	[Parameter(Mandatory = true)]
+	public required Blog Blog { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() =>
+		WriteObject(new Client(ApiKey, Blog).VerifyKey());
 }
